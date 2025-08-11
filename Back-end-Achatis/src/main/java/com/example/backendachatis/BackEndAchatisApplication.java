5cee2a1dc558a8c5dbb193231af4f76f51b6f001
Abdellah824 <arroudabdellah824@gmail.com>
2025-08-11 14:41:53 +0100

    Update Front-end

---
+++ Back-end-Achatis/src/main/java/com/example/backendachatis/BackEndAchatisApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendachatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndAchatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndAchatisApplication.class, args);
+    }
+
+}
---
+++ Back-end-Achatis/src/test/java/com/example/backendachatis/BackEndAchatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendachatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndAchatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
